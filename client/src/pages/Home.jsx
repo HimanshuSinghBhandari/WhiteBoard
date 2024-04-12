e17c259c395fc08d8e38bcbc9794be86db703318
Himanshu Singh Bhandari <himanshubhandari4675@gmail.com>
2024-04-12 14:57:05 +0530

    adding open source button link

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { handleError } from "../utils/handleError";
 import axios from "axios";
 import Particles from "./particles";
+import {Link} from "react-router-dom";
 axios.defaults.withCredentials = true;
 
 export default function Home() {
@@ -59,15 +60,29 @@ export default function Home() {
     <div className="relative max-w-6xl min-h-screen px-4 mx-auto sm:px-6 flex flex-col items-center justify-center">
     <Particles />
     <div className="pt-32 pb-16 md:pt-52 md:pb-32 text-center">
-    <h1 className="pb-4 font-extrabold text-sm lg:text-3xl leading-tight text-transparent bg-clip-text bg-gradient-to-r from-zinc-200/60 via-zinc-200 to-zinc-200/60">
-    <span className="text-xl lg:text-7xl block">Digital whiteboard</span>  
-    <span className="text-lg md:text-xl lg:text-2xl">Visualize your thoughts and ideas</span>
-    </h1>
-    {/* <p className="mb-8 text-lg text-zinc-300">
-      Draw your ideas 
-    </p> */}
-   </div>
+    <div className="mb-6">
+  <div className="relative inline-flex before:absolute before:inset-0">
+    <Link
+      className="bg-zinc-800 text-zinc-300 hover:bg-zinc-700 hover:text-zinc-200 rounded-full py-2 px-4 transition duration-150 ease-in-out"
+      to="https://github.com/HimanshuSinghBhandari/WhiteBoard"
+      target="_blank"
+    >
+      <span className="relative inline-flex items-center">Whiteboard is Open Source</span>
+    </Link>
   </div>
+</div>
+      <h1 className="pb-4 font-extrabold text-3xl sm:text-5xl md:text-3xl lg:text-7xl leading-tight text-transparent bg-clip-text bg-gradient-to-r from-zinc-200/60 via-zinc-200 to-zinc-200/60">
+        Digital whiteboard
+        <span className="text-lg sm:text-2xl md:text-xl lg:text-2xl block">Visualize your thoughts and ideas</span>
+      </h1>
+      <p className="mb-8 text-lg text-zinc-300 animate-fade">
+        <span className="inline-block px-2 py-1  rounded-md">
+          Draw your ideas
+        </span>
+      </p>
+    </div>
+  </div>
+  
   )}
 </div>
   );
